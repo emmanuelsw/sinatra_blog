ce746630248e3195d63f83fa88d02ef805d33970
EmmanuelSW <edhgt89@hotmail.com>
2017-07-28 02:28:12 -0500

    Article format, comments design

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,4 +6,12 @@ class Article < ActiveRecord::Base
   def created_date
     created_at.strftime('%d/%m/%Y')
   end
+
+  def body_formatted
+    if body[-1] == '.' and body.length <= 480
+      body.slice(0..480).concat('..')
+    else
+      body.slice(0..480).concat('...')
+    end
+  end
 end
